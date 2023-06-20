a0abfe3f7ef6515e7155950fe3682393c8764452
Pinpunyu <pe25617259@gmail.com>
2023-06-20 20:44:43 +0800

    data inference

--- utils/data_process.py
+++ utils/data_process.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import shutil
 import cv2
 import torch
+import numpy as np
 
 def tiftopng():
     images = Path("./data/hubmap-hacking-the-human-vasculature/train/image").glob("*.tif")
@@ -27,13 +28,31 @@ def move():
         shutil.copy(file_source,file_destination)
         # exit(0)
 
+def img_showmask(img, mask):
 
+    # img = cv2.imread(imgpath)
+    # mask = cv2.imread(maskpath)
 
+    black = np.array([0, 0, 0])
+    for i in range(0,mask.shape[0]):
+        for j in range(0,mask.shape[1]):
+            if np.all(np.equal(mask[i][j], black)) == False:
+                img[i][j] = mask[i][j]
+                # print(f"{i} {j} {mask[i][j]}")
+
+    # cv2.imshow("img", img)
+    # cv2.waitKey()
+    return img
 
 
 if __name__ == '__main__':
 
-    move()
+    # move()
+    imgpath = './data/hubmap-hacking-the-human-vasculature/train/ori_image/0a1d277fb473.png'
+    maskpath = './data/hubmap-hacking-the-human-vasculature/train/mask/0a1d277fb473.png'
+    img = cv2.imread(imgpath)
+    mask = cv2.imread(maskpath)
+    img_showmask(img, mask)
     # boxes = torch.as_tensor([[183.,   0., 511., 511.]])
     # print(boxes)
     # area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
